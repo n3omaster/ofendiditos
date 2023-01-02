5268f63a88a2877dcdce8e96757728bc52bd0fef
Erich Garcia <ceo@qvapay.com>
2023-01-01 21:37:26 -0500

    Cleanup unused components

--- src/components/OffenseCard.js
+++ src/components/OffenseCard.js
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { useHitView } from '../hooks/useHitView'
+import React from 'react'
+// import { useHitView } from '../hooks/useHitView'
 
 // Wouter Router like React Router
 import { Link } from "wouter";
